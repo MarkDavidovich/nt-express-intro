91c3e83b4880a861d4e155850c9cd571b43dc6bc
Mark <mark.davidovich@gmail.com>
2026-02-24 21:41:05 +0200

    add money to client side and refactor functions to work with it

--- dist/main.js
+++ dist/main.js
@@ -6,44 +6,80 @@ const buyItemInput = document.querySelector(".item-buy");
 const buyItemBtn = document.querySelector(".buy-btn");
 const boughtText = document.querySelector(".bought-text");
 
-checkPriceBtn.addEventListener("click", async () => {
-  const itemName = checkPriceInput.value;
+const moneyText = document.querySelector(".money");
 
-  if (!itemName) return;
+let balance = 2000;
+
+const updateBalance = () => {
+  moneyText.innerText = ` $${balance}`;
+};
+
+const getPrice = async (itemName, isRendered = true) => {
+  if (!itemName) {
+    return;
+  }
 
   try {
     const response = await fetch(`http://localhost:3000/priceCheck/${itemName}`);
     const data = await response.json();
 
     if (response.status === 404) {
-      priceText.innerText = data.error;
-    } else {
+      if (isRendered) {
+        priceText.innerText = data.error;
+      }
+
+      return null;
+    }
+
+    if (isRendered) {
       priceText.innerText = `The price of ${itemName} is $${data.price}`;
     }
+    return data.price;
   } catch (err) {
-    console.error(`Could not fetch data: ${err}`);
-    priceText.innerText = "Could not connect to the server";
+    if (isRendered) {
+      priceText.innerText = "Connection error.";
+    }
+    return null;
   }
-});
+};
 
-buyItemBtn.addEventListener("click", async () => {
-  const itemName = buyItemInput.value;
+const buyItem = async (itemName) => {
+  const itemPrice = await getPrice(itemName, false);
 
-  if (!itemName) return;
+  if (itemPrice === null) {
+    boughtText.innerText = "That item doesn't exist!";
+    return;
+  }
+
+  if (balance < itemPrice) {
+    boughtText.innerText = "You're too broke for this. Get a job!";
+    return;
+  }
 
   try {
-    const response = await fetch(`http://localhost:3000/buy/${itemName}`, {
-      method: "POST",
-    });
+    const response = await fetch(`http://localhost:3000/buy/${itemName}`, { method: "POST" });
     const data = await response.json();
 
-    if (response.status === 404 || response.status === 400) {
+    if (!response.ok) {
       boughtText.innerText = data.error;
     } else {
-      boughtText.innerText = `Congratulations, you've just bought ${data.name} for ${data.price}. There are ${data.inventory} left now in the store.`;
+      boughtText.innerText = `Congratulations, you've just bought ${data.name} for $${data.price}. There are ${data.inventory} left now in the store.`;
+      balance -= data.price;
+      updateBalance();
     }
   } catch (err) {
-    console.error(`Could not fetch data: ${err}`);
-    boughtText.innerText = "Could not connect to the server";
+    boughtText.innerText = "Connection error.";
   }
+};
+
+checkPriceBtn.addEventListener("click", () => {
+  const itemName = checkPriceInput.value;
+  getPrice(itemName);
 });
+
+buyItemBtn.addEventListener("click", () => {
+  const itemName = buyItemInput.value;
+  buyItem(itemName);
+});
+
+updateBalance();
